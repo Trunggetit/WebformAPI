1d9292d23f5c1cdb08f6339176ddac03b4a10f11
Trung The Master <tng333@gmail.com>
2018-04-04 22:44:04 -0400

    Add project files.

---
+++ Trung.Webform/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Trung.Webform
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string apiEndPoint = @"https://jsonplaceholder.typicode.com/posts";
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(apiEndPoint);
+            request.Method = "GET";
+            request.ContentType = "application/json; charset=utf-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader streamReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(0));
+
+            string result = streamReader.ReadToEnd();
+            Response.Write(result);
+        }
+    }
+}
---
+++ Trung.Webform/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trung.Webform.Global" Language="C#" %>
